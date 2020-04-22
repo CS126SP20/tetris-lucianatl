a374776f717a3ffb54f588e409a98e2f6e177259
lucianatl <Csc3maat>
2020-04-21 23:36:35 -0500

    linked final poject with pretzelgui

--- apps/my_app.cc
+++ apps/my_app.cc
@@ -3,19 +3,40 @@
 #include "my_app.h"
 
 #include <cinder/app/App.h>
+#include "pretzel/PretzelGui.h"
 
 
 namespace myapp {
 
 using cinder::app::KeyEvent;
 
-MyApp::MyApp() { }
+MyApp::MyApp() {
 
-void MyApp::setup() { }
+}
+
+void MyApp::setup() {
+    mRadius = 25;
+    gui = pretzel::PretzelGui::create("Tetris settings");
+    gui->setSize(cinder::vec2(10, 10));
+    gui->setPos(cinder::vec2(0,0));
+
+    gui->addSlider("Tetris Size", &mRadius, 2, 1000);
+
+    gui->addSaveLoad();
+    gui->loadSettings();
+
+    ci::gl::enableAlphaBlending();
+
+}
 
 void MyApp::update() { }
 
-void MyApp::draw() { }
+void MyApp::draw() {
+    cinder::gl::clear(cinder::Color(84. / 255., 166. / 255, 1));
+    cinder::gl::drawSolidCircle(cinder::vec2(getWindowCenter()), mRadius);
+
+    gui->draw();
+}
 
 void MyApp::keyDown(KeyEvent event) { }
 
